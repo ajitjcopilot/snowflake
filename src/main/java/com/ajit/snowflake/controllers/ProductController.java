65bb13ad56e4b5d5153bc1b078d1809303a3f4e2
ajitjcopilot <ajitjaiswal@gmail.com>
2025-02-08 07:46:46 +0530

    added logger

--- src/main/java/com/ajit/snowflake/controllers/ProductController.java
+++ src/main/java/com/ajit/snowflake/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.ajit.snowflake.controllers;
 
 import com.ajit.snowflake.models.Product;
 import com.ajit.snowflake.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api/products")
+@Slf4j
 public class ProductController {
 
 
@@ -26,7 +28,7 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
 
-
+        log.info("Productcontroleer.createProduct called..." );
         return new ResponseEntity<>(productService.createProduct(product), HttpStatus.OK);
 
     }
